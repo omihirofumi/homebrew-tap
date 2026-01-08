62e78d88b25d0b92e403643d942a4c3bc28b70bb
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-01-08 20:33:00 +0900

    chore: update v0.5.0 ww

--- Formula/ww.rb
+++ Formula/ww.rb
@@ -1,8 +1,8 @@
 class Ww < Formula
   desc "ww command"
   homepage "https://github.com/omihirofumi/ww"
-  url "https://github.com/omihirofumi/ww/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "086fbd1b0b0a2248c68121a43586f37167e04e73fb3bfb0d2e2ab9bc6047d2a9"
+  url "https://github.com/omihirofumi/ww/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "92fde7849e5c6e44a74ba246ba36a78688d5c7eca9bf941eb9acc3efedd0af97"
   license "MIT"
 
   depends_on "zig" => :build
